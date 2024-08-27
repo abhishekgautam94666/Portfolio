5c7e4c4f6c413323c5ddf146b54e78f24a6013dd
Abhishek <abhishekgautam94666@gmail.com>
2024-08-27 22:46:14 +0530

    add send message

--- app.js
+++ app.js
@@ -1,23 +1,24 @@
 const express = require("express");
-const bodyparser = require("body-parser");
 const nodemailer = require("nodemailer");
 const dotenv = require("dotenv");
-const { log } = require("console");
-const path = require("path");
 
 const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 app.use(express.static("css"));
-app.use(bodyparser.urlencoded({ extended: true }));
 
 dotenv.config({
-  path:"./data/config.env",
-})
+  path: "./data/config.env",
+});
 
 app.get("/", function (req, res) {
   res.sendFile(__dirname + "/index.html");
 });
 
 app.post("/", function (req, res) {
+  
+
   const comm = req.body.message;
   const name = req.body.name;
   const number = req.body.number;
@@ -47,13 +48,13 @@ app.post("/", function (req, res) {
   transport.sendMail(mailOption, function (err, info) {
     if (err) {
       console.log(err);
+      res.json({ success: false, message: err.message });
     } else {
-      res.send("send seccesfully");
+      res.json({ success: true, message: "Email send" });
     }
   });
 });
 
 app.listen(process.env.PORT, function () {
   console.log(`server started at ${process.env.PORT}`);
-  console.log(process.env.PASSWORD);
 });
